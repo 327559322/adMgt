00f689512fb7f7cc14b92ea37d473231f87583b9
傅成涵 <fuchenghan@51yryc.com>
2020-10-30 13:55:38 +0800

    添加表格

--- src/antd.js
+++ src/antd.js
@@ -1,8 +1,10 @@
-import { Button, Layout, Menu } from "ant-design-vue";
+import { Button, Layout, Menu, Table, Modal } from "ant-design-vue";
 
 export function initAntd(app) {
   app
     .use(Button)
+    .use(Table)
     .use(Layout)
+    .use(Modal)
     .use(Menu);
 }
--- src/views/newAd.vue
+++ src/views/newAd.vue
@@ -1,32 +1,97 @@
 <template>
-  <div class="hello">
-    newAd
+  <div>
+    <a-button type="primary" @click="popWin">新建广告</a-button>
+    <div class="table-content">
+      <a-table :columns="columns" :data-source="tableData">
+        <template v-slot:action>
+          <a-button type="primary">统计</a-button>
+          <a-button type="primary">编辑</a-button>
+          <a-button type="danger">删除</a-button>
+        </template>
+      </a-table>
+    </div>
+    <a-modal
+      title="Title"
+      v-model:visible="visible"
+      :confirm-loading="confirmLoading"
+      @ok="handleOk"
+    >
+      <p>123</p>
+    </a-modal>
   </div>
 </template>
 
 <script>
+import { reactive, toRefs } from "vue";
+const data = reactive({
+  visible: false,
+  confirmLoading: false,
+  columns: [
+    {
+      title: "广告名称",
+      dataIndex: "name",
+      key: "name"
+    },
+    {
+      title: "广告地址",
+      dataIndex: "address",
+      key: "address"
+    },
+    {
+      title: "操作",
+      key: "action",
+      slots: { customRender: "action" }
+    }
+  ],
+  tableData: [
+    {
+      key: "1",
+      name: "John Brown",
+      address: "New York No. 1 Lake Park"
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      address: "London No. 1 Lake Park"
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      address: "Sidney No. 1 Lake Park"
+    }
+  ]
+});
 export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  setup() {
+    return {
+      ...toRefs(data),
+      popWin,
+      handleOk
+    };
   }
 };
+
+function popWin() {
+  data.visible = true;
+}
+function handleOk() {
+  data.confirmLoading = true;
+  setTimeout(() => {
+    data.visible = false;
+    data.confirmLoading = false;
+  }, 2000);
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.table-content {
+  margin-top: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.ant-btn {
+  margin-right: 8px;
 }
 </style>
